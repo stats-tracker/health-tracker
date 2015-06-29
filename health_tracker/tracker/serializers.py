625de7acad838001079ae37b7a162b262a46dfa3
Benjamin Phillips <benred42@gmail.com>
2015-06-29 14:52:41 -0400

    Added serializers for Activity and Stat

--- health_tracker/tracker/serializers.py
+++ health_tracker/tracker/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from tracker.models import Activity
+from tracker.models import Activity, Stat
 
 
 class ActivitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Activity
-        fields = ('url', 'title')
+        fields = ('id', 'url', 'title')
+
+
+class StatSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Stat
+        fields = ('id', 'date', 'number')
