ce1a9239e7c4b5eab9771c9ec5c389a66e4ed7c6
BenQuirk <benquirk@yahoo.com>
2015-06-29 15:34:03 -0400

    routing stuff

--- health_tracker/static/js/router.js
+++ health_tracker/static/js/router.js
@@ -11,26 +11,54 @@ function show (content){
 }
 
 // grab our content from the fake scripts and hold in variables
-var homeContent = $('#home').html();
-var pageOneContent = $('#pageOne').html();
-var pageTwoContent = $('#pageTwo').html();
-var pageThreeContent = $('#pageThree').html();
+//var homeContent = $('#activities').html();
+//var pageOneContent = $('#').html();
+//var pageTwoContent = $('#pageTwo').html();
+//var pageThreeContent = $('#pageThree').html();
 
 // the default route, which runs when the url is blank
 router.route('', function () {
-  show(homeContent); // pass content to show function
+
+    $.ajax({
+      url: 'activities/',
+      method: 'GET'
+      // title
+    })
+    .then(getData)
+    .then(renderData);
+
+
+
+   function getData(data) {
+         // grab data from each ajax call here
+         // probably just the title
+         // and the link so we can reference the link in the view/render function?
+
+         id = TBD; // grab id via ajax call
+         title = TBD; // grab title via ajax call
+
+    } // close getData
+
+
+   function renderData(data){
+
+        var mainView = '<a href="activities/"'+id+'/ />'+title+'"</a>"';
+        $('.main-content').append(mainView); // go to main content div and append each item to the DOM
+
+        } // close renderData
+
 });
 
 // all other routes
-router.route('home', function () {
+router.route( '', 'activities', function () {
   show(homeContent); // pass content to show function
 });
 
-router.route('page1', function () {
+router.route('activites/id', function () {
   show(pageOneContent); // pass content to show function
 });
 
-router.route('page2', function () {
+router.route('activities/id/edit', function () {
   show(pageTwoContent); // pass content to show function
 });
 
