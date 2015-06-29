682eb08860e0e9e5c381edc2fe605fe151a1010c
Benjamin Phillips <benred42@gmail.com>
2015-06-29 14:18:15 -0400

    Added a model for Activity and for Stat

--- health_tracker/tracker/models.py
+++ health_tracker/tracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Activity(models.Model):
+    title = models.CharField(max_length=255)
+
+class Stat(models.Model):
+    date = models.DateField()
+    activity = models.ForeignKey(Activity)
