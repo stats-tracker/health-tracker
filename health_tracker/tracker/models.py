50b52f70829d0327a16025602e6e590b69ecdaee
Benjamin Phillips <benred42@gmail.com>
2015-06-29 14:54:04 -0400

    Fixed admin plural name for Activity model

--- health_tracker/tracker/models.py
+++ health_tracker/tracker/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Activity(models.Model):
     title = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = "activities"
+
 class Stat(models.Model):
     date = models.DateField()
     number = models.IntegerField()
