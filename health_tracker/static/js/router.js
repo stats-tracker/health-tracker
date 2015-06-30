e0728b52a861d1102caae1ea4844050f90fd309a
BenQuirk <benquirk@yahoo.com>
2015-06-30 15:26:00 -0400

    details routing working

--- health_tracker/static/js/router.js
+++ health_tracker/static/js/router.js
@@ -1,13 +1,8 @@
 // Create a new instance of the backbone router object
 var router = new Backbone.Router();
 
-// Start Backbone history a necessary step for bookmarkable URL's
-Backbone.history.start();
-
 // the default route, which runs when the url is blank
 router.route('', function () {
-
-
 
 	$.ajax({
 		url: '/activities/',
@@ -19,18 +14,19 @@ router.route('', function () {
 	function testFunction (data){
 		console.log(data);
     for (x = 0; x < data.length; x++)
-					$('.activity-container').append('<a href="activities/'+data[x].id+'" class="activity-tab">'+data[x].title+'</a>');
+					$('.activity-container').append('<a href="#activities/'+data[x].id+'" class="activity-tab">'+data[x].title+'</a>');
   }
 
 }); // end get ajax call
 
 
 
-		// add input form to main page
-		var inputHTML = '<form method="post">{% csrf_token %}<input type="text" placeholder="New Activity" class="add-activity-text" id="inputID"><div class="add-activity-button">+ add</div></form>';
+// add input form to main page
+var inputHTML = '<form action="." method="post"><input type="text" placeholder="New Activity" class="add-activity-text" id="inputID"><div class="add-activity-button">+ add</div></form>';
 
 
-//		$('.add-activity-container').append(inputHTML);
+//
+$('.add-activity-container').append(inputHTML);
 
 
 // get value from user input, pass to postInput function
@@ -67,7 +63,41 @@ function postInput(input){
 
 
 
+// DETAILS ROUTE
+router.route('activities/:id', function (id) {
+	$.ajax({
+	    url: '/activities/'+id,
+	    method: 'GET'
+	  })
+	  .then(renderData);
+
+	  function renderData(record) {
+			console.log(record.title);
+			var pageTitle = record.title;
+			$('.main-content').html(pageTitle);
+
+	  }
+
+	  function renderDatzzza(databaseArray) {
+	    var item = _.findWhere(databaseArray, { id: detailsId });   //HOW TO KNOW/SET IT?
+	    // NOTE bridgeId IS THE SAME AS VAR AS PASSED IN THE ROUTE
+	    // --- IS IT THE SAME THING  ACTUALY OR JUST A COINCIDENCE?
+	    var detailsTemplate = views['details-view'];
+	    var templateFn = _.template(detailsTemplate, { variable: 'm' });
+	    var tempHTML = templateFn(item);
+
+	    $('.main-content').html(tempHTML);
+
+	    renderCharts(item, databaseArray);   // RENDER CHART!
+	  }
 
 
-// backbbone stuff
-Backbone.history.loadUrl();
+
+});
+
+
+
+
+
+// Start Backbone history a necessary step for bookmarkable URL's
+Backbone.history.start();
